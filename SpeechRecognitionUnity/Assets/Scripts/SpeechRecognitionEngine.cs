668316d8f49d1d0428c9de4442dabde1842e80e7
Vangos <pterneas@gmail.com>
2017-02-12 22:58:15 +0200

    Added a speech recognition simple 2D game.

---
+++ SpeechRecognitionUnity/Assets/Scripts/SpeechRecognitionEngine.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class SpeechRecognitionEngine : MonoBehaviour
+{
+    public string[] keywords = new string[]
+    {
+        "up",
+        "down",
+        "left",
+        "right"
+    };
+
+    public ConfidenceLevel confidence = ConfidenceLevel.Medium;
+
+    public float speed = 1;
+
+    public Text results;
+    public Image target;
+
+    protected PhraseRecognizer recognizer;
+    protected string word = "right";
+
+    private void Start()
+    {
+        if (keywords != null)
+        {
+            recognizer = new KeywordRecognizer(keywords, confidence);
+            recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+            recognizer.Start();
+        }
+    }
+
+    private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        word = args.text;
+        results.text = "You said: <b>" + word + "</b>";
+    }
+
+    private void Update()
+    {
+        var x = target.transform.position.x;
+        var y = target.transform.position.y;
+
+        switch (word)
+        {
+            case "up":
+                y += speed;
+                break;
+            case "down":
+                y -= speed;
+                break;
+            case "left":
+                x -= speed;
+                break;
+            case "right":
+                x += speed;
+                break;
+        }
+
+        target.transform.position = new Vector3(x, y, 0);
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (recognizer != null && recognizer.IsRunning)
+        {
+            recognizer.Stop();
+        }
+    }
+}
